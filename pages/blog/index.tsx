ef1806fc698bf314967bf4319de032d47d27b53b
conner1reimers <66189525+conner1reimers@users.noreply.github.com>
2021-08-20 14:19:30 -0500

    .

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -8,8 +8,17 @@ const Blog: React.FC = () => {
           <p>
             For soundshare, I wanted to create a simple webapp for music creators to share different sounds with each other. Not only can you discover sounds and then download them, I also
             decided to implement social media influenced functions, such as liking sounds, reposting sounds, following users, and more. I also have functions in the works for adding effects to sounds.
-            The idea was pretty straightforward, though I had a lot to learn about database design and web design, which I {`didn't`} have much experience in, before coming to this conclusion. After 6 months of hard work I have
-            a working prototype!
+          
+          </p>
+          <br/>
+          <p>
+            The idea was pretty straightforward, though I had a lot to learn about database design and web design, which I {`didn't`} have much experience in, before coming to this conclusion. After months of hard work I had
+            a working prototype to build from!
+            
+          </p>
+          <br/>
+          <p>Now that the site is live all kinds of audio creators, be that music producers, audio engineers, game developers, sound designers, and more can upload and download sounds that they would like
+            to use in their work for free! 
           </p>
         </div>
         
